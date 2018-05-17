92f010bb14a197690b58d596736428adb36ed529
Nikita <losnikita1995@gmail.com>
2018-05-17 22:25:41 +0300

    simplify logging aspect

--- OnlineVisaCenter/src/main/java/com/vironit/onlinevisacenter/aspects/LoggingAspect.java
+++ OnlineVisaCenter/src/main/java/com/vironit/onlinevisacenter/aspects/LoggingAspect.java
@@ -1,11 +1,11 @@
 package com.vironit.onlinevisacenter.aspects;
 
-import com.vironit.onlinevisacenter.exceptions.DuplicateException;
-import com.vironit.onlinevisacenter.exceptions.service.*;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -16,7 +16,7 @@ public class LoggingAspect {
 
     private Logger logger;
 
-    @After("execution(* com.vironit.onlinevisacenter.service.ApplicationServiceImpl.*(..))")
+    @AfterReturning("execution(* com.vironit.onlinevisacenter.service..*(..))")
     public void logApplicationService(JoinPoint joinPoint){
         String currentMethod = joinPoint.getSignature().toShortString();
         String currentClass = joinPoint.getTarget().getClass().toString();
@@ -24,90 +24,8 @@ public class LoggingAspect {
         logger.info(currentMethod + " of " + currentClass + " was execute successfully");
     }
 
-    @After("execution(* com.vironit.onlinevisacenter.service.CountryServiceImpl.*(..))")
-    public void logCountryService(JoinPoint joinPoint){
-        String currentMethod = joinPoint.getSignature().toShortString();
-        String currentClass = joinPoint.getTarget().getClass().toString();
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.info(currentMethod + " of " + currentClass + " was execute successfully");
-    }
-
-    @After("execution(* com.vironit.onlinevisacenter.service.DocumentServiceImpl.*(..))")
-    public void logDocumentService(JoinPoint joinPoint){
-        String currentMethod = joinPoint.getSignature().toShortString();
-        String currentClass = joinPoint.getTarget().getClass().toString();
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.info(currentMethod + " of " + currentClass + " was execute successfully");
-    }
-
-    @After("execution(* com.vironit.onlinevisacenter.service.EmailSenderServiceImpl.*(..))")
-    public void logSenderService(JoinPoint joinPoint){
-        String currentMethod = joinPoint.getSignature().toShortString();
-        String currentClass = joinPoint.getTarget().getClass().toString();
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.info(currentMethod + " of " + currentClass + " was execute successfully");
-    }
-
-    @After("execution(* com.vironit.onlinevisacenter.service.UserServiceImpl.*(..))")
-    public void logUserService(JoinPoint joinPoint){
-        String currentMethod = joinPoint.getSignature().toShortString();
-        String currentClass = joinPoint.getTarget().getClass().toString();
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.info(currentMethod + " of " + currentClass + " was execute successfully");
-    }
-
-    @After("execution(* com.vironit.onlinevisacenter.service.VisaServiceImpl.*(..))")
-    public void logVisaService(JoinPoint joinPoint){
-        String currentMethod = joinPoint.getSignature().toShortString();
-        String currentClass = joinPoint.getTarget().getClass().toString();
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.info(currentMethod + " of " + currentClass + " was execute successfully");
-    }
-
-    @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service.CountryServiceImpl.*(..))",throwing = "e")
-    public void logCountryServiceException(JoinPoint joinPoint, CountryServiceException e){
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.error(e.getMessage(),e);
-    }
-
-    @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service.DocumentServiceImpl.*(..))",throwing = "e")
-    public void logDocumentServiceException(JoinPoint joinPoint,DocumentServiceException e){
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.error(e.getMessage(),e);
-    }
-
-    @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service.EmailSenderServiceImpl.*(..))",throwing = "e")
-    public void logEmailSenderException(JoinPoint joinPoint, SenderServiceException e){
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.error(e.getMessage(),e);
-    }
-
-    @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service.UserServiceImpl.*(..))",throwing = "e")
-    public void logUserServiceException(JoinPoint joinPoint,UserServiceException e){
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.error(e.getMessage(),e);
-    }
-
-    @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service.VisaServiceImpl.*(..))",throwing = "e")
-    public void logVisaServiceException(JoinPoint joinPoint,VisaServiceException e){
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.error(e.getMessage(),e);
-    }
-
-    @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service.ApplicationServiceImpl.*(..))",throwing = "e")
-    public void logApplicationServiceException(JoinPoint joinPoint,ApplicationServiceException e){
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.error(e.getMessage(),e);
-    }
-
-    @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service.UserServiceImpl.*(..))",throwing = "e")
-    public void logLoginException(JoinPoint joinPoint,LoginationException e){
-        logger = LogManager.getLogger(joinPoint.getTarget().getClass());
-        logger.error(e.getMessage(),e);
-    }
-
     @AfterThrowing(value = "execution(* com.vironit.onlinevisacenter.service..*(..))",throwing = "e")
-    public void logDuplicateException(JoinPoint joinPoint,DuplicateException e){
+    public void logServiceException(JoinPoint joinPoint, Exception e){
         logger = LogManager.getLogger(joinPoint.getTarget().getClass());
         logger.error(e.getMessage(),e);
     }
