b548b16e56402c8b342f8f0b1578921fd119110a
Nikita <losnikita1995@gmail.com>
2018-05-17 10:54:50 +0300

    decompose methods in email service, add mail.properties

--- OnlineVisaCenter/src/main/java/com/vironit/onlinevisacenter/service/EmailSenderServiceImpl.java
+++ OnlineVisaCenter/src/main/java/com/vironit/onlinevisacenter/service/EmailSenderServiceImpl.java
@@ -3,6 +3,9 @@ package com.vironit.onlinevisacenter.service;
 import com.vironit.onlinevisacenter.entity.Application;
 import com.vironit.onlinevisacenter.exceptions.service.SenderServiceException;
 import com.vironit.onlinevisacenter.service.interfaces.SenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.mail.*;
@@ -11,41 +14,63 @@ import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 @Service
+@PropertySource("classpath:mail.properties")
 public class EmailSenderServiceImpl implements SenderService {
 
-    private final String username = "losnikita1995@gmail.com";
-    private final String password = "Novopolotsk13051995";
+    private Environment environment;
+
+    @Autowired
+    public EmailSenderServiceImpl(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public void sendResultToClient(Application application) throws SenderServiceException {
         String email = application.getUser().getEmail();
-        String message = application.getResult() + application.getComments() +application.getStatus();
-        sendEmail(email,message);
+        String message = "Your application status: " + application.getStatus() + "\n" +
+                         "Your application result: " + application.getResult() + "\n" +
+                         "Your application comments: " + application.getComments();
+        sendMessage(email,message);
     }
 
-    private void sendEmail(String email, String messageText) throws SenderServiceException {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
+    private void sendMessage(String email, String messageText) throws SenderServiceException {
         try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
+            Message message = new MimeMessage(createSession());
+            message.setFrom(new InternetAddress(environment.getProperty("visa_center.username")));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse(email));
-            message.setSubject("Your result of visa application");
+            message.setSubject(environment.getProperty("subject"));
             message.setText(messageText);
             Transport.send(message);
         } catch (MessagingException e) {
-            throw new SenderServiceException("Error of sending email");
+            throw new SenderServiceException("Error of sending email",e);
         }
     }
+
+    private Properties setUpProperties(){
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", environment.getProperty("auth"));
+        properties.put("mail.smtp.starttls.enable", environment.getProperty("starttls"));
+        properties.put("mail.smtp.host", environment.getProperty("host"));
+        properties.put("mail.smtp.port", environment.getProperty("port"));
+        return properties;
+    }
+
+    private Session createSession(){
+        return Session.getInstance(setUpProperties(),
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(environment.getProperty("visa_center.username"),
+                                environment.getProperty("visa_center.pass"));
+                    }
+                });
+    }
+
 }
+
+
+
+
+
+
+
